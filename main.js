aecffaaa4113906e2c20f6fbf42931d2d29bfa27
Ye Li <157327792+ywyeli@users.noreply.github.com>
2024-03-11 09:53:51 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+var mobileMenuBtn = document.querySelector("#mobile-menu-btn");
+var mobileMenu = document.querySelector(".mobile-menu");
+
+mobileMenuBtn.addEventListener("click", () => {
+  if (mobileMenu.style.display === "none") {
+    mobileMenu.style.display = "flex";
+    mobileMenuBtn.innerHTML = "Close";
+  } 
+  else {
+    mobileMenu.style.display = "none";
+    mobileMenuBtn.innerHTML = "Menu";
+  }
+});
